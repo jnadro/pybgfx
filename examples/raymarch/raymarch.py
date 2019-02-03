aeaa42deb0e623f8472f2644a13db30a3899210c
jnadro <jason.nadro@gmail.com>
2019-02-03 15:54:47 -0600

    [RayMarch] - Create vertex decl

--- examples/raymarch/raymarch.py
+++ examples/raymarch/raymarch.py
@@ -2,6 +2,16 @@ import ctypes
 import pybgfx as bgfx
 
 
+class PosColorTexCoord0Vertex(ctypes.Structure):
+    _fields_ = [
+        ("m_x", ctypes.c_float),
+        ("m_y", ctypes.c_float),
+        ("m_z", ctypes.c_float),
+        ("m_abgr", ctypes.c_uint32),
+        ("m_u", ctypes.c_float),
+        ("m_v", ctypes.c_float)
+    ]
+
 class Raymarch(bgfx.App):
 
     def __init__(self, width, height, title):
@@ -19,6 +29,18 @@ class Raymarch(bgfx.App):
         bgfx.set_debug(bgfx.BGFX_DEBUG_TEXT)
         bgfx.set_view_clear(0, bgfx.BGFX_CLEAR_COLOR |
                             bgfx.BGFX_CLEAR_DEPTH, 0x303030ff, 1.0, 0)
+
+        # Create vertex stream declaration.
+        rendererType = bgfx.get_renderer_type()
+        self.ms_decl = bgfx.vertex_decl()
+        bgfx.vertex_decl_begin(ctypes.byref(self.ms_decl), rendererType)
+        bgfx.vertex_decl_add(self.ms_decl, bgfx.BGFX_ATTRIB_POSITION,
+                             3, bgfx.BGFX_ATTRIB_TYPE_FLOAT, False, False)
+        bgfx.vertex_decl_add(self.ms_decl, bgfx.BGFX_ATTRIB_COLOR0,
+                             4, bgfx.BGFX_ATTRIB_TYPE_UINT8, True, False)
+        bgfx.vertex_decl_add(self.ms_decl, bgfx.BGFX_ATTRIB_TEXCOORD0,
+                             2, bgfx.BGFX_ATTRIB_TYPE_FLOAT, False, False)
+        bgfx.vertex_decl_end(self.ms_decl)
 
     def shutdown(self):
         bgfx.shutdown()
